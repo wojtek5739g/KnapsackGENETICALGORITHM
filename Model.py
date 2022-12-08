77e576ab206b231166c17836f8bea1249a3b8673
adam8a42 <90727602+adam8a42@users.noreply.github.com>
2022-12-08 17:06:56 +0100

    changed fitnessFunction to return 1/ret

--- Model.py
+++ Model.py
@@ -1,18 +1,18 @@
 import numpy as np
 class Model:
     city_coordinates = [] # [(x,y), ...]
-    population = [] #[1,2,3,...]
+    population = [] #[[1,2,3,...],...]
     def __init__(self) -> None:
         pass
     def distanceFunction(self,xy1, xy2):
-        x1,y1 = xy1
-        x2,y2 = xy2
+        x1, y1 = xy1
+        x2, y2 = xy2
         return np.sqrt((x2-x1)**2 + (y2-y1)**2)
     def fitnessFunction(self, specimen):
         ret = 0
         for city1, city2 in zip(specimen[:-1], specimen[1:]):
             ret += self.distanceFunction(self.city_coordinates[city1], self.city_coordinates[city2])
-        return ret
+        return 1/ret
 
 
 
