d87e3eb37d7a0804706dde60e9290fc5343800a9
adam8a42 <90727602+adam8a42@users.noreply.github.com>
2022-12-08 16:29:27 +0100

    Modified distance function to read tuples and tested fitenss function - it works

--- Model.py
+++ Model.py
@@ -4,13 +4,16 @@ class Model:
     population = [] #[1,2,3,...]
     def __init__(self) -> None:
         pass
-    def distanceFunction(x1, y1, x2,y2):
+    def distanceFunction(self,xy1, xy2):
+        x1,y1 = xy1
+        x2,y2 = xy2
         return np.sqrt((x2-x1)**2 + (y2-y1)**2)
     def fitnessFunction(self, specimen):
         ret = 0
         for city1, city2 in zip(specimen[:-1], specimen[1:]):
             ret += self.distanceFunction(self.city_coordinates[city1], self.city_coordinates[city2])
         return ret
+
 
 
         
