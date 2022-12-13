1942a8dbd4bff7d1aab38d77c533b63a64009efe
wojtek5739g <wojtek5739@gmail.com>
2022-12-13 19:03:06 +0100

    class related changes to Model

--- Model.py
+++ Model.py
@@ -1,13 +1,23 @@
 import numpy as np
+
 class Model:
     city_coordinates = [] # [(x,y), ...]
     population = [] #[[1,2,3,...],...]
-    def __init__(self) -> None:
-        pass
-    def distanceFunction(self,xy1, xy2):
+    def __init__(self, city_coordinates, population):
+        self._city_coordinates = city_coordinates
+        self._population = population
+
+    def distanceFunction(self, xy1, xy2):
         x1, y1 = xy1
         x2, y2 = xy2
         return np.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+    def get_city_coordinates(self):
+        return city_coordinates
+
+    def get_population(self):
+        return population
+
     def fitnessFunction(self, specimen):
         ret = 0
         for city1, city2 in zip(specimen[:-1], specimen[1:]):
@@ -16,7 +26,7 @@ class Model:
 
 
 
-        
-        
+
+
 
 
