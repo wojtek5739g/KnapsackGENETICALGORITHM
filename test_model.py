c4ce7103ac4c796329efcff3af83c853402c9205
adam8a42 <90727602+adam8a42@users.noreply.github.com>
2022-12-31 12:39:02 +0100

    added tests for visualization functions

--- test_model.py
+++ test_model.py
@@ -1,7 +1,10 @@
 from Model import Model
-
+from Visualizer import visualise_specimen, plot_progress
 def test_model():
     model = Model([(0, 2), (5, 4), (120, 4), (56, 42)], [[1, 2], [2, 3]])
     assert model.fitnessFunction(model.get_population()[0]) == 115.0
 
-
+def test_specimen():
+    visualise_specimen([(0, 2), (5, 4), (120, 4), (56, 42)], [3,1,2,0])
+def test_progress_visualization():
+    plot_progress([(0, 2), (5, 4), (120, 4), (56, 42)], [[0,1,2,3],[3,1,2,0]])
