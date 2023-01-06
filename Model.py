19444e7d2d4b3b7d7dfb4004cfaef6a6f24983e1
adam8a42 <90727602+adam8a42@users.noreply.github.com>
2023-01-06 13:54:29 +0100

    Added crossover, seems to work

--- Model.py
+++ Model.py
@@ -5,10 +5,12 @@ class Model:
     _city_coordinates = [] # [(x,y), ...]
     _population = [] #[[1,2,3,...], [3, 7, 6, ...], ...]
     _mutation_coefficient = 0
-    def __init__(self, city_coordinates, population, mutation_coefficient):
+    _crossover_coefficient = 0
+    def __init__(self, city_coordinates, population, mutation_coefficient, crossover_coefficient):
         self._city_coordinates = city_coordinates
         self._population = population
         self._mutation_coefficient = mutation_coefficient
+        self._crossover_coefficient = crossover_coefficient
 
     def distance_function(self, xy1, xy2):
         x1, y1 = xy1
@@ -26,7 +28,6 @@ class Model:
                 b = random.randint(0,len(specimen))
                 self._population[i,a],  self._population[i,b] = self._population[i,b],  self._population[i,a]
 
-
     def get_city_coordinates(self):
         return self._city_coordinates
 
@@ -39,6 +40,36 @@ class Model:
             ret += self.distance_function(self.get_city_coordinates()[city1], self.get_city_coordinates()[city2])
         return ret
 
+    def crossover(self, specimen_a, specimen_b):
+        a = random.randint(0,len(specimen_a))
+        b = random.randint(a,len(specimen_a))
+        specimen_a[a:b], specimen_b[a:b] = specimen_b[a:b], specimen_a[a:b]
+        set_a = set()
+        set_b = set()
+        doubles_a = []
+        doubles_b = []
+        for i,j in zip(specimen_a, specimen_b):
+            if i not in set_a:
+                set_a.add(i)
+            else:
+                doubles_a.append(i)
+            if j not in set_b:
+                set_b.add(j)
+            else:
+                doubles_b.append(j)
+        n_changed = 0
+        for i, val_i in enumerate(specimen_a):
+            if i < a or i >= b:
+                if val_i in doubles_a:
+                    specimen_a[i] = doubles_b[n_changed]
+                    n_changed += 1
+        n_changed = 0
+        for i, val_i in enumerate(specimen_b):
+            if i < a or i >= b:
+                if val_i in doubles_b:
+                    specimen_b[i] = doubles_a[n_changed]
+                    n_changed += 1
+        return (specimen_a, specimen_b)
 
 
 
--- test_model.py
+++ test_model.py
@@ -1,10 +1,14 @@
 from Model import Model
 from Visualizer import visualise_specimen, plot_progress
 def test_model():
-    model = Model([(0, 2), (5, 4), (120, 4), (56, 42)], [[1, 2], [2, 3]])
+    model = Model([(0, 2), (5, 4), (120, 4), (56, 42)], [[1, 2], [2, 3]],0,0)
     assert model.fitnessFunction(model.get_population()[0]) == 115.0
 
 def test_specimen():
     visualise_specimen([(0, 2), (5, 4), (120, 4), (56, 42)], [3,1,2,0])
 def test_progress_visualization():
     plot_progress([(0, 2), (5, 4), (120, 4), (56, 42)], [[0,1,2,3],[3,1,2,0]])
+
+def test_crossover():
+    model = Model([(0, 2), (5, 4), (120, 4), (56, 42),(10, 2), (20, 4), (30, 4), (40, 42)], [[0, 1,2,3], [1, 2,0,3]],0,0)
+    print(model.crossover(model._population[0], model._population[1]))
