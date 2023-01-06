64ff77004a285697df86493dbcc4849d2308bd8f
adam8a42 <90727602+adam8a42@users.noreply.github.com>
2023-01-06 15:59:46 +0100

    fixed bugs

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from Model import Model
 from Visualizer import visualise_specimen, plot_progress
 def main():
-    city_coordinates = []
-    model = Model([(0, 2), (5, 4), (120, 4), (56, 42),(10, 2), (20, 4), (30, 4), (40, 42)],[],0.01,0.5)
-    model.generate_population()
-    best_fits = [model.get_best_specimen]
-    for i in range(10000):
+    city_coordinates = [(0, 2), (5, 4), (120, 4), (56, 42),(10, 2), (20, 4), (30, 4), (40, 42)]
+    model = Model(city_coordinates,[],0.01,0.5)
+    model.generate_population(100)
+    print(model._population)
+    best_fits = [model.get_best_specimen()]
+    for i in range(1000):
         model.create_offspring()
         model.mutate()
         best_fits.append(model.get_best_specimen())
-    visualise_specimen(best_fits[-1])
-    plot_progress(best_fits)
+        print(f"iteration {i}")
+    visualise_specimen(city_coordinates, best_fits[-1])
+    plot_progress(city_coordinates,best_fits)
+    print(best_fits)
 
 
 if __name__ == "__main__":
