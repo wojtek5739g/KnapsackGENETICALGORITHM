22430a58602487e3f3e4c078f191b59524cdd9ee
adam8a42 <90727602+adam8a42@users.noreply.github.com>
2023-01-15 17:44:12 +0100

    Replaced the greedy algorithm

--- GDAlgorithm.py
+++ GDAlgorithm.py
@@ -5,15 +5,41 @@ from Visualizer import visualise_specimen, plot_progress
 import sys
 import argparse
 
-def GDAlgorithmComp(model, cities_coordinates, num_of_individuals, num_of_iterations):
-    model.generate_population(num_of_individuals)
-    best_fits = np.array([model.get_best_specimen()])
-    for i in range(num_of_iterations):
-        model.generate_population(num_of_individuals)
-        best_specimen = model.get_best_specimen()
-        best_fits = np.append(best_fits, [best_specimen], axis = 0)
-        print(f"iteration {i}: Distance: {1/model.fitness_function(best_specimen)}")
-    return model.fitness_function(best_fits[-1])
+#def GDAlgorithmComp(model, cities_coordinates, num_of_individuals, num_of_iterations):
+#    model.generate_population(num_of_individuals)
+#    best_fits = np.array([model.get_best_specimen()])
+#    for i in range(num_of_iterations):
+#        model.generate_population(num_of_individuals)
+#        best_specimen = model.get_best_specimen()
+#        best_fits = np.append(best_fits, [best_specimen], axis = 0)
+#        print(f"iteration {i}: Distance: {1/model.fitness_function(best_specimen)}")
+#    return model.fitness_function(best_fits[-1])
+
+def GDAlgorithmComp(cities_coordinates):
+    model = Model(cities_coordinates, [])
+    best_specimen = np.array([i for i in range(len(cities_coordinates))])
+    for i_start,start in enumerate(cities_coordinates):
+        specimen = np.array([],dtype=int)
+        specimen = np.append(specimen, i_start)
+        location = start
+        visited = set()
+        visited.add(i_start)
+        for i in range(len(cities_coordinates)):
+            distances = sorted([model.distance_function(location,j) for j in cities_coordinates])
+            for j, j_dist in enumerate(distances):
+                if j not in visited:
+                    specimen = np.append(specimen, j)
+                    visited.add(j)
+                    location = cities_coordinates[j]
+                    break
+        print(specimen)
+        if model.fitness_function(specimen) > model.fitness_function(best_specimen):
+            best_specimen = specimen
+    #print(1/model.fitness_function(best_specimen))
+    return model.fitness_function(best_specimen)
+        
+            
+
 
 def GDAlgorithm(num_of_cities, max_X_coord_value, max_Y_coord_value, num_of_individuals, num_of_iterations):
     cities_coordinates = map_generation(num_of_cities, max_X_coord_value, max_Y_coord_value)
