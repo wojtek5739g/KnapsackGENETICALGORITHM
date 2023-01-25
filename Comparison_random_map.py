66c47bbf12c8376f55f175d89162415854d45bfb
adam8a42 <90727602+adam8a42@users.noreply.github.com>
2023-01-26 00:47:08 +0100

    random map comaprison

---
+++ Comparison_random_map.py
@@ -0,0 +1,48 @@
+from GDAlgorithm import GDAlgorithmComp
+from GeneticAlgorithm import GeneticAlgorithmComp
+from Visualizer import comparison_fitness_function_plot, comparison_distance_plot
+from Generate_map import map_generation
+from Model import Model
+import argparse
+import sys
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Launch Genetic and DQ Algorithm comparison')
+    parser.add_argument('num_of_launches', type=int, help='Number of launching the comparison')
+    parser.add_argument('num_of_cities', type=int, help='Number of cities')
+    parser.add_argument('max_X_coord_value', type=int, help='Maximum value of X coordinate')
+    parser.add_argument('max_Y_coord_value', type=int, help='Maximum value of Y coordinate')
+    parser.add_argument('num_of_individuals', type=int, help='Number of individuals (series of different orders of cities in population')
+    parser.add_argument('num_of_iterations', type=int, help='Number of iterations of the algorithm')
+    parser.add_argument('mutation_coef', type=float, help='Mutation coefficient')
+    parser.add_argument('crossover_coef', type=float, help='Crossover coefficient')
+    parser.add_argument('selection', choices=['elitism', 'roulette', 'tournament'], type=str, help='Selection type')
+    parser.add_argument('--tournament_size', nargs='?', type=int, help='Tournament size')
+    args = parser.parse_args()
+
+    best_specimensGEN = [] #fitness_function_values
+    best_specimensDistanceGEN = [] #distance_values
+    best_specimensGA = [] #fitness_function_values
+    best_specimensDistanceGA = [] #distance_values
+    best_specimenDistanceGEN_min = []
+    for i in range(args.num_of_launches):
+        cities_coordinates = map_generation(args.num_of_cities, args.max_X_coord_value, args.max_Y_coord_value)
+        GAmodel = Model(cities_coordinates, [], args.mutation_coef, args.crossover_coef, args.tournament_size)
+        print(f"{i+1} Launch: ")
+        GAbestSpecimen,GAbestSpecimen_max = GeneticAlgorithmComp(GAmodel, cities_coordinates, args.num_of_individuals, args.num_of_iterations, args.mutation_coef, args.crossover_coef, args.tournament_size, args.selection)
+        GDbestSpecimen = GDAlgorithmComp(cities_coordinates)
+        best_specimensGEN.append(GAbestSpecimen)
+        best_specimensDistanceGEN.append(1/GAbestSpecimen)
+        best_specimensGA.append(GDbestSpecimen)
+        best_specimensDistanceGA.append(1/GDbestSpecimen)
+        best_specimenDistanceGEN_min.append(1/GAbestSpecimen_max)
+    comparison_fitness_function_plot(args.num_of_launches, best_specimensGEN, best_specimensGA,
+                                    args.num_of_cities, args.num_of_individuals, args.num_of_iterations,
+                                    args.mutation_coef, args.crossover_coef, args.selection, args.tournament_size)
+    comparison_distance_plot(args.num_of_launches, best_specimensDistanceGEN, best_specimensDistanceGA, best_specimenDistanceGEN_min,
+                            args.num_of_cities, args.num_of_individuals, args.num_of_iterations,
+                            args.mutation_coef, args.crossover_coef, args.selection, args.tournament_size)
+        
+
+if __name__ == "__main__":
+    main(sys.argv)
